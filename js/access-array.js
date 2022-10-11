c90e9be5f2babb9f63a958b22c91451a519c3d7f
Carlo Contardi <c.contardi@icloud.com>
2022-10-11 17:19:36 +0200

    array update added

--- js/access-array.js
+++ js/access-array.js
@@ -1,5 +1,5 @@
 var colors=['Purple','Green','Orange'];
-
+// si ricorda che il primo elemento di un array ha indice 0
 colors[0]='Violet';
 var elColor=document.getElementById("color");
 elColor.textContent=colors[0];
