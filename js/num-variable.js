e18a06f7e248979909b214a1f8ae077a72492ee3
Carlo Contardi <c.contardi@icloud.com>
2022-10-11 16:17:28 +0200

    str var added

---
+++ js/num-variable.js
@@ -0,0 +1,7 @@
+var price=5;
+var seats=23;
+var total;
+total=price*seats;
+console.log(total);
+var element= document.getElementById("total");
+element.textContent="Totale: " +total+" Euro";
---
+++ js/str-variable.js
@@ -0,0 +1,8 @@
+var username;
+var message;
+username="Prometeus";
+message="buon CODING spaziale ";
+var elUsername=document.getElementById("username");
+elUsername.textContent=username;
+var elMessage=document.getElementById("message");
+elMessage.textContent=message;
