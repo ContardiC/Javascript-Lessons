c506aadb8fe8f90977cd1d1a154e59e1444e236d
ContardiC <c.contardi@icloud.com>
2022-10-27 18:28:49 +0200

    object literal added

--- js/object-literal.js
+++ js/object-literal.js
@@ -3,6 +3,9 @@ var user = {
     lastName : 'Rossi',
     YOB : 2000,
     age : function(){
-        return Date().getFullYear() - this.YOB;
+        var currentYear= new Date().getFullYear();
+        return  currentYear - this.YOB;
     }
 } 
+var elUserData=document.getElementById("user-data");
+elUserData.textContent="Nome: " + user.firstName + " Cognome: " + user.lastName + " Età: " + user.age();
