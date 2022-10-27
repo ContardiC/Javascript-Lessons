54053e8846bd5e8ea873d13336606fff3e04a1da
ContardiC <c.contardi@icloud.com>
2022-10-27 18:32:46 +0200

    Constructor added

---
+++ js/object-constructor.js
@@ -0,0 +1,14 @@
+
+
+function User(firstName,lastName,YOB){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.YOB = YOB;
+    this.age = function(){
+        var currentYear= new Date().getFullYear();
+        return  currentYear - this.YOB;
+    };
+}
+var user=new User("Mario","Rossi",2000);
+var elUserData=document.getElementById("user-data");
+elUserData.textContent="Nome: " + user.firstName + " Cognome: " + user.lastName + " Età: " + user.age();
