c3a6033950a329fcf7059f068e8b977d7753372f
ContardiC <c.contardi@icloud.com>
2022-10-29 15:56:42 +0200

    target.tagName

--- js/event-object.js
+++ js/event-object.js
@@ -1,6 +1,7 @@
 function targetInfo(e){
     var elResponse = document.getElementById('response');
-    elResponse.textContent= e.srcElement;
+    elResponse.innerHTML= "Tipo evento " + e.type;
+    elResponse.innerHTML+="<br>Nel TAG: "+e.target.tagName;
 }
 var elUsername=document.getElementById('username');
 elUsername.addEventListener('focus',targetInfo);
