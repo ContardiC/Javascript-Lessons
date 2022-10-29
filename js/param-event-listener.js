8a21807829d62106d9eb07e01240221516f0c9f5
ContardiC <c.contardi@icloud.com>
2022-10-29 14:20:05 +0200

    param-event-listener added

--- js/param-event-listener.js
+++ js/param-event-listener.js
@@ -1,14 +1,13 @@
+var elUsername= document.getElementById('username');
+elUsername.addEventListener('blur',function(){
+    checkUsername('#');
+    console.log('Evento blur');
+},false);
 function checkUsername(symbol){
     var elResponse = document.getElementById('response');
-    var username = this.value;
-    if(username.charAt(0)==symbol){
+    if(elUsername.value.charAt(0)==symbol){
         elResponse.textContent='Il nome utente non può iniziare con #';
     }else{
         elResponse.textContent='';
     }
 }
-var elUsername= document.getElementById('username');
-elUsername.addEventListener('blur',function(){
-    checkUsername('#');
-    console.log('controllo eseguito');
-},false);
